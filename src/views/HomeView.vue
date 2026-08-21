32e218e78c51352f49df5524962c90743f5923af
Jatin Sonavane <95110624+spacecricket@users.noreply.github.com>
2026-08-21 13:40:13 -0700

    Tweak language on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,8 @@
     <p className="leading-7 text-justify">
       Slack and its clones are noisy. They clutter our workspace
       and make it hard to tell what's important. Legato helps teams stay focused
-      by providing an experience that protects attention and encourages depth in
-      communication. If your teams find chat distracting,
-      Legato is being built for you.
+      by protecting attention and encouraging depth in communication.
+      If your teams find chat distracting, Legato is being built for you.
     </p>
   </div>
   <div class="flex flex-row items-center justify-center p-16">
